e1d50fe5200fdc8b46aff27c242666738af1bbaf
Leonardo Webster <leonardowebster15@gmail.com>
2018-09-27 16:20:38 -0300

    adicionando menu responsivo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,4 +1,3 @@
-<!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
@@ -16,7 +15,7 @@
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Parisienne" rel="stylesheet">
-
+    {{-- <link href="https://getbootstrap.com/docs/4.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"> --}}
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
@@ -24,99 +23,75 @@
 </head>
 <body>
     <div id="app">
-        {{-- <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+        <div class="menu menu-dark">
+            <nav class="navbar navbar-dark navbar-expand-lg w-100 nav-pills nav-fill">
+                <a class="navbar-brand logo sliding-middle-out item-menu" href="{{ url('/home') }}" style="font-size: 25pt;text-transform: initial;">
+                    <span class="circle laraColor">#</span> Laravel
                 </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
+                
+                <button type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="true" aria-label="Toggle navigation" class="navbar-toggler"><span class="navbar-toggler-icon"></span></button>
+                
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            <li class="nav-item">
-                                @if (Route::has('register'))
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                @endif
-                            </li>
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+                        <li class="nav-item nav-link active">
+                            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item nav-link ">
+                            <a class="nav-link" href="#">Link1</a>
+                        </li>
+                        <li class="nav-item nav-link  dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Dropdown
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="#">
+                                    Action
                                 </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav> --}}
-
-        <div class="menu-interno">
-            @if (Route::has('login'))
-                <div class="row row-simples">
-                    <div class="col-md-2 links w-100 position-menu-left logo" style="text-align: center;">
-                        <a class="sliding-middle-out item-menu" style="font-size: 31pt;text-transform: initial;" href="{{ url('/home') }}">(='X'=)</a>
-                    </div>
-                    <div class="col-md-8 links w-100 position-menu-left menu-item">
-                        <a class="sliding-middle-out item-menu" href="{{ url('/home') }}">TEsteLogo</a>
-                        <a class="sliding-middle-out item-menu" href="{{ url('/home') }}">TEsteLogo</a>
-                        <a class="sliding-middle-out item-menu" href="{{ url('/home') }}">TEsteLogo</a>
-                        <a class="sliding-middle-out item-menu" href="{{ url('/home') }}">TEsteLogo</a>
-                        <a class="sliding-middle-out item-menu" href="{{ url('/home') }}">TEsteLogo</a>
-                        <a class="sliding-middle-out item-menu" href="{{ url('/home') }}">TEsteLogo</a>
-                        <a class="sliding-middle-out item-menu" href="{{ url('/home') }}">TEsteLogo</a>
-                    </div>
-                    <div class="col-md-2 links w-100 position-menu-left dropdown-menu-item">
-                        <div class="btn-group" role="group">
-                            <button id="user-id-button" type="button" class="btn btn-secondary dropdown-toggle especial-button-dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <img src="{{Gravatar::get(Auth::user()->email)}}" class="m--img-rounded m--marginless user-pic" alt=""/>
-                                <span class="user-details menu-item">
-                                    {{ Auth::user()->name }}
-                                </span>
-                            </button>
-                            <div class="dropdown-menu" aria-labelledby="user-id-button">
-                                <a class="dropdown-item" href="#">Configurações</a>
-                                <a class="dropdown-item" href="{{ route('logout') }}"
-                                   onclick="event.preventDefault();
-                                                 document.getElementById('logout-form').submit();">
-                                    {{ __('Sair') }}
+                                <a class="dropdown-item" href="#">
+                                    Another action
                                 </a>
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                    @csrf
-                                </form>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" href="#">
+                                    Something else here
+                                </a>
                             </div>
+                        </li>
+                        <li class="nav-item nav-link ">
+                            <a class="nav-link" href="#">Link2</a>
+                        </li>
+                        <li class="nav-item nav-link ">
+                            <a class="nav-link" href="#">Link3</a>
+                        </li>
+                        <li class="nav-item nav-link ">
+                            <a class="nav-link disabled" href="#">
+                                Disabled
+                            </a>
+                        </li>
+                    </ul>
+                    <div class="btn-group" role="group">
+                        <button id="user-id-button" type="button" class="btn btn-secondary dropdown-toggle especial-button-dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <img src="{{Gravatar::get(Auth::user()->email)}}" class="m--img-rounded m--marginless user-pic" alt=""/>
+                            <span class="user-details menu-item">
+                                {{ Auth::user()->name }}
+                            </span>
+                        </button>
+                        <div class="dropdown-menu" aria-labelledby="user-id-button">
+                            <a class="dropdown-item" href="#">Configurações</a>
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                               onclick="event.preventDefault();
+                                             document.getElementById('logout-form').submit();">
+                                {{ __('Sair') }}
+                            </a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
                         </div>
                     </div>
                 </div>
-            @endif
+            </nav>
         </div>
 
-
-        <main class="py-4 content">
+        <main class="py-4 mt-7 content">
             @yield('content')
         </main>
     </div>
--- resources/views/page/welcome.blade.php
+++ resources/views/page/welcome.blade.php
@@ -1,4 +1,3 @@
-<!doctype html>
 <html lang="{{ app()->getLocale() }}">
     <head>
         <meta charset="utf-8">
@@ -74,7 +73,6 @@
             .top-left.links.w-100.position-menu-left a {
                 color: #fff;
             }
-            
         </style>
     </head>
     <body>
@@ -243,57 +241,74 @@
                     <span class="sr-only">Next</span>
                 </a>
             </div>
-            <div class="menu">
-                @if (Route::has('login'))
-                    <div class="row row-simples">
-                        <div class="col-md-2 links w-100 position-menu-left logo" style="text-align: center;">
-                            <a class="sliding-middle-out item-menu" style="font-size: 31pt;text-transform: initial;" href="{{ url('/home') }}">(='X'=)</a>
-                        </div>
-                        <div class="col-md-8 links w-100 position-menu-left menu-item">
-                            <a class="sliding-middle-out item-menu" href="{{ url('/home') }}">TEsteLogo</a>
-                            <a class="sliding-middle-out item-menu" href="{{ url('/home') }}">TEsteLogo</a>
-                            <a class="sliding-middle-out item-menu" href="{{ url('/home') }}">TEsteLogo</a>
-                            <a class="sliding-middle-out item-menu" href="{{ url('/home') }}">TEsteLogo</a>
-                            <a class="sliding-middle-out item-menu" href="{{ url('/home') }}">TEsteLogo</a>
-                            <a class="sliding-middle-out item-menu" href="{{ url('/home') }}">TEsteLogo</a>
-                            <a class="sliding-middle-out item-menu" href="{{ url('/home') }}">TEsteLogo</a>
-                        </div>
-                        <div class="col-md-2 links w-100 position-menu-left dropdown-menu-item">
-                            <div class="btn-group" role="group">
-                                <button id="user-id-button" type="button" class="btn btn-secondary dropdown-toggle especial-button-dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    <img src="{{Gravatar::get(Auth::user()->email)}}" class="m--img-rounded m--marginless user-pic" alt=""/>
-                                    <span class="user-details menu-item">
-                                        {{ Auth::user()->name }}
-                                    </span>
-                                </button>
-                                <div class="dropdown-menu" aria-labelledby="user-id-button">
-                                    <a class="dropdown-item" href="#">Configurações</a>
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Sair') }}
+            <div class="menu menu-dark">
+                <nav class="navbar navbar-dark navbar-expand-lg w-100 nav-pills nav-fill">
+                    <a class="navbar-brand logo sliding-middle-out item-menu" href="{{ url('/home') }}" style="font-size: 25pt;text-transform: initial;">
+                        <span class="circle laraColor">#</span> Laravel
+                    </a>
+                    
+                    <button type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="true" aria-label="Toggle navigation" class="navbar-toggler"><span class="navbar-toggler-icon"></span></button>
+                    
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav mr-auto">
+                            <li class="nav-item nav-link active">
+                                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                            </li>
+                            <li class="nav-item nav-link ">
+                                <a class="nav-link" href="#">Link1</a>
+                            </li>
+                            <li class="nav-item nav-link  dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Dropdown
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="#">
+                                        Action
                                     </a>
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
+                                    <a class="dropdown-item" href="#">
+                                        Another action
+                                    </a>
+                                    <div class="dropdown-divider"></div>
+                                    <a class="dropdown-item" href="#">
+                                        Something else here
+                                    </a>
                                 </div>
+                            </li>
+                            <li class="nav-item nav-link ">
+                                <a class="nav-link" href="#">Link2</a>
+                            </li>
+                            <li class="nav-item nav-link ">
+                                <a class="nav-link" href="#">Link3</a>
+                            </li>
+                            <li class="nav-item nav-link ">
+                                <a class="nav-link disabled" href="#">
+                                    Disabled
+                                </a>
+                            </li>
+                        </ul>
+                        <div class="btn-group" role="group">
+                            <button id="user-id-button" type="button" class="btn btn-secondary dropdown-toggle especial-button-dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <img src="{{Gravatar::get(Auth::user()->email)}}" class="m--img-rounded m--marginless user-pic" alt=""/>
+                                <span class="user-details menu-item">
+                                    {{ Auth::user()->name }}
+                                </span>
+                            </button>
+                            <div class="dropdown-menu" aria-labelledby="user-id-button">
+                                <a class="dropdown-item" href="#">Configurações</a>
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                                 document.getElementById('logout-form').submit();">
+                                    {{ __('Sair') }}
+                                </a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    @csrf
+                                </form>
                             </div>
                         </div>
                     </div>
-                @endif
+                </nav>
             </div>
-            {{-- <div class="menu">
-                @if (Route::has('login'))
-                    <div class="top-left links w-100 position-menu-left">
-                        @auth
-                            <a class="sliding-middle-out item-menu" href="{{ url('/home') }}">Home</a>
-                        @else
-                            <a class="sliding-middle-out item-menu" href="{{ route('login') }}">Login</a>
-                            <a class="sliding-middle-out item-menu" href="{{ route('register') }}">Registro</a>
-                        @endauth
-                    </div>
-                @endif
-            </div> --}}
+
         </div>
         <div class="p-lg-5 mt-3" style="color: #000;">
             
